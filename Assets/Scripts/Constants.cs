1b879b8a1c7525e7f650350ab8e568444546d5b3
Matt Llanes <55604455+llanesm@users.noreply.github.com>
2021-10-29 23:44:27 -0600

    added script for instantiating/moving nodes. Cleanup/renaming

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts
+{
+    public class Constants
+    {
+        public const int OriginDepth = 300;
+        public const int TerminationDepth = 0;
+        public const float InterCardinalRadius = 1.31F;
+        public const float InterCardinalRotation = 47;
+        public const float MovementSpeed = -0.1F;
+        public const int Interval = 2;
+    }
+}
---
+++ Assets/Scripts/NodeStartPoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class NodeStartPoint
+    {
+        public string Name { get; set; }
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
+        public float RotX { get; set; }
+        public float RotY { get; set; }
+        public float RotZ { get; set; }
+        public Vector3 Transform { get; set; }
+        public Quaternion Rotation { get; set; }
+
+        public NodeStartPoint(string name, float posX, float posY, float posZ, float rotX, float rotY, float rotZ)
+        {
+            Name = name;
+            PosX = posX;
+            PosY = posY;
+            PosZ = posZ;
+            RotX = rotX;
+            RotY = rotY;
+            RotZ = rotZ;
+            Transform = new Vector3(posX, posY, posZ);
+            Rotation = Quaternion.Euler(rotX, rotY, rotZ);
+        }
+    }
+}
